f8124ec180b55d7f6228d2ceb99a05ff30248906
shachijain08 <shachi.jain2003@gmail.com>
2022-11-05 17:50:27 -0400

    Update RadioDriver.py

--- EosPayload/drivers/RadioDriver.py
+++ EosPayload/drivers/RadioDriver.py
@@ -1,1 +1,7 @@
-class DriverBase
+from EosPayload.lib.driver_base import DriverBase
+import logging
+class RadioDriver(DriverBase):
+    def device_read(self, logger: logging.Logger) -> None:
+        return 0
+    def device_command(self, logger: logging.Logger) -> None:
+        return 0;
