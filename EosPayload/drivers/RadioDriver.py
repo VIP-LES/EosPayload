c4a77d3a129d90b6ed1d99ce06c5ac2a966de997
David-Rey <davidreynolds0809@gmail.com>
2022-11-07 21:09:07 -0500

    Radio Driver semi-complete without queue.

--- EosPayload/drivers/RadioDriver.py
+++ EosPayload/drivers/RadioDriver.py
@@ -2,56 +2,53 @@ import datetime
 
 from EosPayload.lib.driver_base import DriverBase
 from digi.xbee.devices import XBeeDevice
+from digi.xbee.devices import RemoteXBeeDevice
+from digi.xbee.devices import XBee64BitAddress
 import logging
 import time
 
-
-import EosLib.packet.definitions
-import EosLib.packet.packet
-import EosLib.packet.transmit_header
+from EosPayload.lib import MQTT_HOST
+from EosPayload.lib.mqtt import Topic
+from EosPayload.lib.mqtt.client import Client
 
 PORT = "COM1"
+
 
 class RadioDriver(DriverBase):
 
     global PORT
 
+    def setup(self) -> None:
+        self.port = XBeeDevice(PORT, 9600)
+        self.port.open()
+        self.remote = RemoteXBeeDevice(self.port, XBee64BitAddress.from_hex_string("0013A20040XXXXXX"))
+
+
+    @staticmethod
+    def get_device_id() -> str:
+        return "radio-driver-007"
+
     def device_read(self, logger: logging.Logger) -> None:
-
-        device = XBeeDevice(PORT, 9600)
-
-        device.open()
+        mqtt = Client(MQTT_HOST)
 
         def data_receive_callback(xbee_message):
-            print("From %s >> %s" % (xbee_message.remote_device.get_64bit_addr(),
-                                     xbee_message.data.decode()))
+            packet = xbee_message.decode()
+            mqtt.send(Topic.HEALTH_HEARTBEAT, packet)
 
-            packet_transmit_header = xbee_message.data.decode()
-
-        device.add_data_received_callback(data_receive_callback)
+        self.port.add_data_received_callback(data_receive_callback)
 
         while True:
             time.sleep(1)
 
         return 0
+
     def device_command(self, logger: logging.Logger) -> None:
+        mqtt = Client(MQTT_HOST)
 
+        def send_via_xbee(transmit_packet):
+            self.port.send_data_async(self.remote, transmit_packet)
 
+        mqtt.register_subscriber(Topic.RADIO_TRANSMIT, send_via_xbee)
 
         return 0
-
-'''
-        message = EosLib.packet.packet.Packet()
-        message.data_header = EosLib.packet.packet.DataHeader()
-
-        message.data_header.data_type = EosLib.packet.definitions.Type.TELEMETRY
-        message.data_header.priority = EosLib.packet.definitions.Priority.DATA
-        message.data_header.sender = EosLib.packet.definitions.Device.GPS
-        message.data_header.generate_time = datetime.datetime.now()
-
-        message.body = str("All this for a string ?!")
-
-        message.body = message.body.encode()
-        # why only encode the body? Why not the entire message?
-'''
 
