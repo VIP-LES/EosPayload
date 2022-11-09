e992f04b23e59d72ae07423a9bf9214753cc4226
Jiacheng Zhang <jiachengzhang0128@gmail.com>
2022-11-09 18:02:14 -0500

    Add address

--- EosPayload/drivers/Pressure.py
+++ EosPayload/drivers/Pressure.py
@@ -1,17 +1,8 @@
 import logging
 import time
 from random import randint
-
+from smbus2 import SMBus
 from EosPayload.lib.driver_base import DriverBase
-
-"AIN4", "P9_33"
-"AIN6", "P9_35"
-"AIN5", "P9_36"
-"AIN2", "P9_37"
-"AIN3", "P9_38"
-"AIN0", "P9_39"
-"AIN1", "P9_40"
-
 
 
 class PressureDriver(DriverBase):
@@ -24,14 +15,8 @@ class PressureDriver(DriverBase):
         logger.info("Starting to poll for data!")
         while True:
             # this is where you would poll a device for data or whatever
-            data = randint(0, 256)
-            self.data_log([str(data), str(data * data)])
-            time.sleep(3)
-
-    def device_command(self, logger: logging.Logger) -> None:
-        logger.info("Starting to send a command!")
-        while True:
-            # this is where you would send a command to device
-            data = randint(0, 256)
-            self.data_log([str(data), str(data * data)])
+            bus = SMBus(2)
+            b = bus.read_i2c_block_data(0x76, 0x88, 24)
+            str_b = list(map(str, b))
+            self.data_log(str_b)
             time.sleep(3)
--- EosPayload/drivers/Temp&Humidity.py
+++ EosPayload/drivers/Temp&Humidity.py
@@ -2,7 +2,7 @@ import logging
 import time
 
 from random import randint
-
+from smbus2 import SMBus
 from EosPayload.lib.driver_base import DriverBase
 
 
@@ -15,9 +15,11 @@ class TempHumidity(DriverBase):
     def device_read(self, logger: logging.Logger) -> None:
         logger.info("Starting to poll for data!")
         while True:
-            # this is where you would poll a device for data
-            data = randint(0, 256)
-            self.data_log([str(data), str(data * data)])
+            # this is where you would poll a device for data or whatever
+            bus = SMBus(2)
+            b = bus.read_i2c_block_data(0x44, 0x88, 24)
+            str_b = list(map(str, b))
+            self.data_log(str_b)
             time.sleep(3)
     def device_command(self, logger: logging.Logger) -> None:
         self.spin()
