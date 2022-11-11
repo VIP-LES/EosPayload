f18959fbd9ee490f9640e27034610f185e442986
Thomas Holder <tholder7@gatech.edu>
2022-11-11 11:45:51 -0500

    Add debugging print

--- EosPayload/drivers/engineering_data_driver.py
+++ EosPayload/drivers/engineering_data_driver.py
@@ -48,6 +48,7 @@ class EngineeringDataDriver(PositionAwareDriverBase):
         data_datetime = datetime.datetime.strptime(data_datetime_string, EngineeringDataDriver.esp_data_time_format)
         data_dict['datetime'] = str(data_datetime.timestamp())
         data_dict['LAT'].replace('N', '').replace('S', '')
+        print(data_dict['LAT'])
         data_dict['LONG'].replace('E', '').replace('W', '').replace('N', '').replace('S', '')
 
         return list_data, data_dict
