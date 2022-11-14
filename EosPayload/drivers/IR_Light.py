cb26e7dccb714ba4b1fd748296d82615d3b64994
jaydenwitucki <72230095+jaydenwitucki@users.noreply.github.com>
2022-11-14 14:54:13 -0500

    updated drivers i2c

--- EosPayload/drivers/IR_Light.py
+++ EosPayload/drivers/IR_Light.py
@@ -17,9 +17,19 @@ class LightIR(DriverBase):
             # this is where you would poll a device for data
             logger.info("Starting to poll for data!")
             bus = SMBus(4)
-            b = bus.read_i2c_block_data(0x29, 0x88, 24)
-            str_b = list(map(str, b))
-            self.data_log(str_b)
+            data = bus.read_i2c_block_data(0x29, 0x88, 24)
+            #str_b = list(map(str, b))
+            #self.data_log(str_b)
+            csv_row = [str(data), str(data * data)]
+            try:
+                self.data_log(csv_row)
+            except Exception as e:
+                logger.error(f"unable to log data : {e}")
+
+            try:
+                self.data_transmit(csv_row)
+            except Exception as e:
+                logger.error(f"unable to transmit data: {e}")
             time.sleep(3)
 
     def device_command(self, logger: logging.Logger) -> None:
