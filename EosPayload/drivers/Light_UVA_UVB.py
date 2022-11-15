fba217863bdabe2d47c10d3a0f9a956d1cafd367
Jiacheng Zhang <jiachengzhang0128@gmail.com>
2022-11-15 12:04:22 -0500

    fix bad merge

--- EosPayload/drivers/Light_UVA_UVB.py
+++ EosPayload/drivers/Light_UVA_UVB.py
@@ -1,16 +1,10 @@
 import logging
 import time
-<<<<<<< HEAD
 from EosPayload.lib.driver_base import DriverBase
 import Adafruit_BBIO.ADC as ADC
 
 ADC.setup()
-=======
 
-from random import randint
-
-from EosPayload.lib.driver_base import DriverBase
->>>>>>> cb26e7dccb714ba4b1fd748296d82615d3b64994
 
 
 # ADC
@@ -23,11 +17,7 @@ class LightUVAUVB(DriverBase):
     def device_read(self, logger: logging.Logger) -> None:
         while True:
             # this is where you would poll a device for data or whatever
-<<<<<<< HEAD
             value = ADC.read_raw("P9_39")
-=======
-            value = 0
->>>>>>> cb26e7dccb714ba4b1fd748296d82615d3b64994
             data = value
             self.data_log([str(data), str(data * data)])
             time.sleep(3)
