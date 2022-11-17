44a8a9639de0f13f11f4604aa6b2ef276ccaf3bf
Jiacheng Zhang <jiachengzhang0128@gmail.com>
2022-11-17 16:46:16 -0500

    set offset to 0

--- EosPayload/drivers/I2C_driver.py
+++ EosPayload/drivers/I2C_driver.py
@@ -25,31 +25,31 @@ class I2CDriver(DriverBase):
             # this is where you would poll a device for data or whatever
             bus = SMBus(2)
             try:
-                th = bus.read_i2c_block_data(0x44, 0x88, 24)  # Temperature & Humidity
+                th = bus.read_i2c_block_data(0x44, 0x00, 24)  # Temperature & Humidity
             except Exception as e:
                 th = -1
                 logger.critical("A fatal exception occurred when attempting to get temp & humidity data"
                                 f": {e}\n{traceback.format_exc()}")
             try:
-                pr = bus.read_i2c_block_data(0x76, 0x88, 24)  # Pressure, alternative address: 0x40
+                pr = bus.read_i2c_block_data(0x76, 0x00, 24)  # Pressure, alternative address: 0x40
             except Exception as e:
                 pr = -1
                 logger.critical("A fatal exception occurred when attempting to get pressure data"
                                 f": {e}\n{traceback.format_exc()}")
             try:
-                pa = bus.read_i2c_block_data(0x69, 0x88, 24)  # Particulates
+                pa = bus.read_i2c_block_data(0x69, 0x00, 24)  # Particulates
             except Exception as e:
                 pa = -1
                 logger.critical("A fatal exception occurred when attempting to get particulates data"
                                 f": {e}\n{traceback.format_exc()}")
             try:
-                irv = bus.read_i2c_block_data(0x29, 0x88, 24)  # Light (IR Visible), alternative address: 0x28
+                irv = bus.read_i2c_block_data(0x29, 0x00, 24)  # Light (IR Visible), alternative address: 0x28
             except Exception as e:
                 irv = -1
                 logger.critical("A fatal exception occurred when attempting to get Light (IR Visible) data"
                                 f": {e}\n{traceback.format_exc()}")
             try:
-                vuva = bus.read_i2c_block_data(0x53, 0x88, 24)  # Light (Visible UVA)
+                vuva = bus.read_i2c_block_data(0x53, 0x00, 24)  # Light (Visible UVA)
             except Exception as e:
                 vuva = -1
                 logger.critical("A fatal exception occurred when attempting to get Light (Visible UVA) data"
