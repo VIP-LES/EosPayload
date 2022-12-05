6fbdaab47c56eee3bdc43ce832a3be7a4f919255
Daryl Dohner <daryl.dohner@gmail.com>
2022-12-05 08:03:33 -0500

    #22 turn off test drivers and broken radio driver

--- EosPayload/drivers/test_driver.py
+++ EosPayload/drivers/test_driver.py
@@ -10,6 +10,9 @@ from EosPayload.lib.driver_base import DriverBase
 
 class TestDriver(DriverBase):
 
+    @staticmethod
+    def enabled() -> bool:
+        return False
 
     @staticmethod
     def get_device_id() -> Device:
