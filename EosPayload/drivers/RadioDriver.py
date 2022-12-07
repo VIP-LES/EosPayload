e8e5ac9bdaae8447ba3ad4afab854c5adff322b2
Jiacheng Zhang <jiachengzhang0128@gmail.com>
2022-12-07 13:10:40 -0500

    radio driver remerge

--- EosPayload/drivers/RadioDriver.py
+++ EosPayload/drivers/RadioDriver.py
@@ -1,39 +1,11 @@
-# from random import randint
-# import logging
-# import time
-# from smbus2 import SMBus
-# from datetime import datetime
-#
-# from EosLib.packet.definitions import Device
-# from EosPayload.lib.driver_base import DriverBase
-#
-#
-# class RadioDriver(DriverBase):
-#     @staticmethod
-#     def get_device_id() -> Device:
-#         return Device.RADIO
-#
-#     @staticmethod
-#     def get_device_name() -> str:
-#         return "RadioDriver"
-#
-#     def device_read(self, logger: logging.Logger) -> None:
-#         return 0
-#
-#     def device_command(self, logger: logging.Logger) -> None:
-#         return 0
 from EosPayload.lib.driver_base import DriverBase
 import logging
-
-
 class RadioDriver(DriverBase):
 
-    @staticmethod
     def enabled() -> bool:
         return False
 
     def device_read(self, logger: logging.Logger) -> None:
         return 0
-
     def device_command(self, logger: logging.Logger) -> None:
         return 0
