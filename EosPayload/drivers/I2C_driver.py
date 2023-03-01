a54cd6bfa722d4f396fe95d5c21447a8604ce0cf
Fiza Shaikh <57720381+fzask@users.noreply.github.com>
2023-03-01 15:49:01 -0500

    again again again

--- EosPayload/drivers/I2C_driver.py
+++ EosPayload/drivers/I2C_driver.py
@@ -28,59 +28,59 @@ class I2CDriver(DriverBase):
     def device_read(self, logger: logging.Logger) -> None:
         logger.info("Starting to poll for data!")
         i2c = board.I2C()
-        # ms = MS8607(i2c)
-        tsl = TSL2591(i2c)
-        ltr = LTR390(i2c)
+        ms = MS8607(i2c)
+        # tsl = TSL2591(i2c)
+        # ltr = LTR390(i2c)
         count = 0
         while True:
 
-            # while True:
-            #     print("Pressure: %.2f hPa" % sensor.pressure)
-            #     print("Temperature: %.2f C" % sensor.temperature)
-            #     print("Humidity: %.2f %% rH" % sensor.relative_humidity)
+            while True:
+                print("Pressure: %.2f hPa" % sensor.pressure)
+                print("Temperature: %.2f C" % sensor.temperature)
+                print("Humidity: %.2f %% rH" % sensor.relative_humidity)
+
+            try:
+                rh = ms.relative_humidity
+                pres = ms.pressure
+                temp = ms.temperature
+                rh_str = str(round(rh, 3))
+                pres_str = str(round(pres, 3))
+                temp_str = str(round(temp, 3))
+            except Exception as e:
+                rh_str = '-1'
+                pres_str = '-1'
+                temp_str = '-1'
+                logger.critical("A fatal exception occurred when attempting to get temp/humidity/pressure data"
+                                f": {e}\n{traceback.format_exc()}")
 
             # try:
-            #     rh = ms.relative_humidity
-            #     pres = ms.pressure
-            #     temp = ms.temperature
-            #     rh_str = str(round(rh, 3))
-            #     pres_str = str(round(pres, 3))
-            #     temp_str = str(round(temp, 3))
+            #     lux = tsl.lux
+            #     infrared = tsl.infrared
+            #     visible = tsl.visible
+            #     full_spectrum = tsl.full_spectrum
+            #     lux_str = str(round(lux, 3))
+            #     infrared_str = str(round(infrared, 3))
+            #     visible_str = str(round(visible, 3))
+            #     full_spectrum_str = str(round(full_spectrum, 3))
             # except Exception as e:
-            #     rh_str = '-1'
-            #     pres_str = '-1'
-            #     temp_str = '-1'
-            #     logger.critical("A fatal exception occurred when attempting to get temp/humidity/pressure data"
+            #     lux_str = '-1'
+            #     infrared_str = '-1'
+            #     visible_str = '-1'
+            #     full_spectrum_str = '-1'
+            #     logger.critical("A fatal exception occurred when attempting to get Light (IR Visible) data"
+            #                     f": {e}\n{traceback.format_exc()}")
+            # try:
+            #     uv = ltr.uvs
+            #     amb_light = ltr.light
+            #     uv_str = str(round(uv, 3))
+            #     amb_light_str = str(round(amb_light, 3))
+            # except Exception as e:
+            #     uv_str = '-1'
+            #     amb_light_str = '-1'
+            #     logger.critical("A fatal exception occurred when attempting to get Light (Visible UVA) data"
             #                     f": {e}\n{traceback.format_exc()}")
 
-            try:
-                lux = tsl.lux
-                infrared = tsl.infrared
-                visible = tsl.visible
-                full_spectrum = tsl.full_spectrum
-                lux_str = str(round(lux, 3))
-                infrared_str = str(round(infrared, 3))
-                visible_str = str(round(visible, 3))
-                full_spectrum_str = str(round(full_spectrum, 3))
-            except Exception as e:
-                lux_str = '-1'
-                infrared_str = '-1'
-                visible_str = '-1'
-                full_spectrum_str = '-1'
-                logger.critical("A fatal exception occurred when attempting to get Light (IR Visible) data"
-                                f": {e}\n{traceback.format_exc()}")
-            try:
-                uv = ltr.uvs
-                amb_light = ltr.light
-                uv_str = str(round(uv, 3))
-                amb_light_str = str(round(amb_light, 3))
-            except Exception as e:
-                uv_str = '-1'
-                amb_light_str = '-1'
-                logger.critical("A fatal exception occurred when attempting to get Light (Visible UVA) data"
-                                f": {e}\n{traceback.format_exc()}")
-
-            csv_row = [lux_str, infrared_str, visible_str, full_spectrum_str, uv_str, amb_light_str]
+            # csv_row = [lux_str, infrared_str, visible_str, full_spectrum_str, uv_str, amb_light_str]
 
             # this saves data to a file
             try:
@@ -101,4 +101,4 @@ class I2CDriver(DriverBase):
 
     @staticmethod
     def enabled() -> bool:
-        return False
+        return True
