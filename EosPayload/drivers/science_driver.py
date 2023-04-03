dcf2de3182f827b6e5f666354912e07562266d80
nathan-tieu <ntieu3@gatech.edu>
2023-04-03 18:56:32 -0400

    bno testing

--- EosPayload/drivers/science_driver.py
+++ EosPayload/drivers/science_driver.py
@@ -8,6 +8,10 @@ from EosLib.device import Device
 from digitalio import DigitalInOut, Direction, Pull
 from adafruit_pm25.i2c import PM25_I2C
 
+# test with bno
+# TODO delete later
+from adafruit_bno055 import BNO055_I2C
+from adafruit_blinka.microcontroller.am335x import pin
 
 class ScienceDriver(DriverBase):
 
@@ -31,12 +35,24 @@ class ScienceDriver(DriverBase):
         reset_pin = None
         i2c = busio.I2C(board.SCL, board.SDA)
         pm25 = PM25_I2C(i2c, reset_pin)
+
+        self.i2c = busio.I2C(pin.I2C1_SCL, pin.I2C1_SDA)
+
+        self.bno = BNO055_I2C(self.i2c)
+
+
         while True:
             time.sleep(1)
 
             try:
-                aqdata = pm25.read()
-                logger.info(aqdata)
+                temperature = self.bno.temperature
+                x_rotation = self.bno.euler[0]
+                y_rotation = self.bno.euler[1]
+                z_rotation = self.bno.euler[2]
+                logger.info("Euler angle: (SCIENCE) {}".format(self.bno.euler))
+                logger.info("Temperature: {} (SCIENCE) degrees C".format(self.bno.temperature))
+                # aqdata = pm25.read()
+                # logger.info(aqdata)
             except RuntimeError:
                 logger.info("Unable to read from sensor, retrying...")
                 logger.info("Hello")
