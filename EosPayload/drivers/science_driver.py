4cb919e0ce805bbf0feffb9aa3d3eb391e77b0c1
nathan-tieu <ntieu3@gatech.edu>
2023-04-04 03:10:50 -0400

    air quality again perhaps??

--- EosPayload/drivers/science_driver.py
+++ EosPayload/drivers/science_driver.py
@@ -42,10 +42,10 @@ class ScienceDriver(DriverBase):
         # bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
 
         # research sensors
-        # reset_pin = None
-        # pm25 = PM25_I2C(i2c, reset_pin)
-        ADC.setup()
-        analogPin = "P9_40"
+        reset_pin = None
+        pm25 = PM25_I2C(i2c, reset_pin)
+        # ADC.setup()
+        # analogPin = "P9_40"
         logger.info("Starting to poll for science data!")
 
 
@@ -68,25 +68,25 @@ class ScienceDriver(DriverBase):
                 # logger.info("Altitude: {} m".format(bmp.altitude))
 
                 # research sensor readout
-                potVal = ADC.read(analogPin)
-                potVolt = potVal * 1.8
-                logger.info("radioactivity: {}".format(potVolt))
+                # potVal = ADC.read(analogPin)
+                # potVolt = potVal * 1.8
+                # logger.info("radioactivity: {}".format(potVolt))
 
 
-                # aqdata = pm25.read()
-                # logger.info("Concentration Units (standard)")
-                # logger.info("---------------------------------------")
-                # logger.info(
-                #     "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
-                #     % (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
-                # )
-                # logger.info("Concentration Units (environmental)")
-                # logger.info("---------------------------------------")
-                # logger.info(
-                #     "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
-                #     % (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
-                # )
-                # logger.info("---------------------------------------")
+                aqdata = pm25.read()
+                logger.info("Concentration Units (standard)")
+                logger.info("---------------------------------------")
+                logger.info(
+                    "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+                    % (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
+                )
+                logger.info("Concentration Units (environmental)")
+                logger.info("---------------------------------------")
+                logger.info(
+                    "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+                    % (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
+                )
+                logger.info("---------------------------------------")
                 # logger.info("Particles > 0.3um / 0.1L air:", aqdata["particles 03um"])
                 # logger.info("Particles > 0.5um / 0.1L air:", aqdata["particles 05um"])
                 # logger.info("Particles > 1.0um / 0.1L air:", aqdata["particles 10um"])
