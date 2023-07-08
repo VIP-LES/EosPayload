4ad9c2ac0224daa009404db738286f4d50f335f0
Thomas Holder <tholder7@gatech.edu>
2023-07-08 06:07:48 -0400

    Update position aware logging to be slightly cheaper

--- EosPayload/drivers/position_aware_logging_driver.py
+++ EosPayload/drivers/position_aware_logging_driver.py
@@ -5,24 +5,16 @@ from EosPayload.lib.base_drivers.position_aware_driver_base import PositionAware
 
 class PositionAwareLoggingDriver(PositionAwareDriverBase):
 
-    def setup(self) -> None:
-        super().setup()
-        self.register_thread('position-logging', self.device_command)
-
-    def device_command(self, logger: logging.Logger) -> None:
-        while True:
-            self.thread_sleep(logger, 10)
-            try:
-                if self.latest_position is not None:
-                    self.data_log([str(self.latest_position.timestamp),
-                                   str(self.latest_position.latitude),
-                                   str(self.latest_position.longitude),
-                                   str(self.latest_position.altitude),
-                                   str(self.latest_position.speed),
-                                   str(self.latest_position.number_of_satellites),
-                                   str(self.latest_position.valid),
-                                   str(self.latest_position.flight_state.name)])
-                else:
-                    self.data_log(["no position"])
-            except Exception as e:
-                logger.error(f"Encountered error trying to log: {e}")
+    def position_callback(self, _client, _userdata, _message):
+        super().position_callback(_client, _userdata, _message)
+        if self.latest_position is not None:
+            self.data_log([str(self.latest_position.timestamp),
+                           str(self.latest_position.latitude),
+                           str(self.latest_position.longitude),
+                           str(self.latest_position.altitude),
+                           str(self.latest_position.speed),
+                           str(self.latest_position.number_of_satellites),
+                           str(self.latest_position.valid),
+                           str(self.latest_position.flight_state.name)])
+        else:
+            self.data_log(["no position"])
