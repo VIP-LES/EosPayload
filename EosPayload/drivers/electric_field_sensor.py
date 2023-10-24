b45b500d313c57c0ab41bbef33eae00b18d1b01e
eaeyal <eeyal3@gatech.edu>
2023-10-24 11:47:45 -0400

    Added time delay

--- EosPayload/drivers/electric_field_sensor.py
+++ EosPayload/drivers/electric_field_sensor.py
@@ -1,4 +1,5 @@
 from EosPayload.lib.base_drivers.driver_base import DriverBase
+import time
 try:
     import Adafruit_BBIO.ADC as ADC
 except ModuleNotFoundError:
@@ -14,5 +15,7 @@ class ElectricFieldSensor(DriverBase):
             value = ADC.read(adc_pin)
             voltage = value * 1.8  # BeagleBone Black has a 1.8V reference voltage
             print(f"ADC Value: {value}, Voltage: {voltage:.2f} V")
+            time.sleep(1)
+
     except Exception as e:
         print(f"An error occurred: {e}")
