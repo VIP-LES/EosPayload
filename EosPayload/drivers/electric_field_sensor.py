94756cb7c3af7529e10ba38afec9c12944de3c40
eaeyal <eeyal3@gatech.edu>
2023-10-27 13:37:20 -0400

    Debugging driver

--- EosPayload/drivers/electric_field_sensor.py
+++ EosPayload/drivers/electric_field_sensor.py
@@ -4,21 +4,22 @@ import logging
 try:
     import Adafruit_BBIO.ADC as ADC
 except ModuleNotFoundError:
+    print("ADC not installed correctly")
     pass
 
 
 class ElectricFieldSensor(DriverBase):
-
-
     def device_read(self, logger: logging.Logger) -> None:
-        adc_pin = "32"
-        ADC.setup()
+        adc_pins = ["P9_36", "P9_38", "P9_40"]
+        for pin in adc_pins:
+            ADC.setup(pin)
         try:
             while True:
                 # Read the voltage from the ADC pin
-                value = ADC.read(adc_pin)
-                voltage = value * 1.8  # BeagleBone Black has a 1.8V reference voltage
-                logger.info(f"ADC Value: {value}, Voltage: {voltage:.2f} V")
+                values = [ADC.read(pin) for pin in adc_pins]
+                voltages = [value * 1.8 for value in values]  # BeagleBone Black has a 1.8V reference voltage
+                for i, pin in adc_pins:
+                    logger.info(f"ADC{pin}, Voltage: {voltages[i]:.2f} V")
                 time.sleep(1)
 
         except Exception as e:
